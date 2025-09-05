439b2cb12dbe19b0b542a5c9830a4dca5ab2ef8e
Lewis-mbui <lewmugo14479@gmail.com>
2025-09-05 21:59:17 +0300

    Do hide credit number exercise

---
+++ level-2-medium/02-hide-credit-card.js
@@ -0,0 +1,29 @@
+const cardNumber = "1234567890123456"
+
+console.log(hideCreditCard(cardNumber));
+
+/*
+function hideCreditCard(cardNumber) {
+  let newStr = "";
+  for (i = 0; i < cardNumber.length; i++) {
+    if (i < 12) newStr += '*';
+    else newStr += cardNumber[i];
+  }
+
+  return newStr;
+}
+*/
+
+/*
+function hideCreditCard(cardNumber) {
+  let result = "";
+  for (let i = 0; i < cardNumber.length; i++) {
+    result += (i < cardNumber.length - 4) ? "*" : cardNumber[i];
+  }
+  return result;
+}
+*/
+
+function hideCreditCard(cardNumber) {
+  return "*".repeat(cardNumber.length - 4) + cardNumber.slice(-4);
+}
