39a9cef4e2fc73704801424de70d81db16ff8d1f
Lewis-mbui <lewmugo14479@gmail.com>
2025-09-24 19:28:19 +0300

    check leap year challenge

---
+++ level-2-medium/15-leap-year.js
@@ -0,0 +1,27 @@
+// Write a function to check if a year is a leap year.
+
+// A leap year is a year that satisfies the following conditions:
+// - If a year is divisible by 4, it could be a leap year.
+// - However, if that year is also divisible by 100, 
+//   it must also be divisible by 400
+
+const year = 2000;
+
+console.log(checkLeapYear(year));
+
+function checkLeapYear(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0))
+    return "Leap";
+
+  if ((year % 100 === 0) && (year % 400 === 0))
+    return "Leap";
+
+  return "Noleap";
+}
+
+/*
+function checkLeapYear(year) {
+  const isLeap = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+  return isLeap ? "Leap" : "Noleap";
+}
+*/
