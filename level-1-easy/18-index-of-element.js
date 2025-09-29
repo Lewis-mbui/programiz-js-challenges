7fa9abdb497c6ee7adbf7b440bc64428b85a3e76
Lewis-mbui <lewmugo14479@gmail.com>
2025-09-29 05:26:52 +0300

    index of element in array challenge

---
+++ level-1-easy/18-index-of-element.js
@@ -0,0 +1,29 @@
+// Write a function to find the index of an element in an array.
+
+// Return the index of element in array  if it is found, 
+// otherwise return -1
+
+const arr = [2, 4, 6, 8, 10];
+const element = 6;
+console.log(findElementIndex(arr, element));
+
+function findElementIndex(arr, element) {
+  return arr.indexOf(element);
+}
+
+/*
+function findElementIndex(arr, element) {
+  let foundIndex = -1;
+
+  for (let i = 0; i < arr.length; i++) {
+    const val = arr[i];
+
+    if (val === element) {
+      foundIndex = i;
+      break;
+    }
+  }
+
+  return foundIndex;
+}
+*/
