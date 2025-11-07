dec805c3b5dff3b370e83dbaa8b4571c530d295b
Lewis-mbui <lewmugo14479@gmail.com>
2025-11-07 18:13:11 +0300

    use ternary operator challenge

---
+++ level-1-easy/37-use-ternary.js
@@ -0,0 +1,8 @@
+// Write a function to return the larger of two numbers 
+// using the ternary operator.
+
+console.log(findLarger(95, 18));
+
+function findLarger(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
