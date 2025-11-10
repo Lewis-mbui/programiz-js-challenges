ec2d6781cb43cc4a581d6fecfaf2f7f144487949
Lewis-mbui <lewmugo14479@gmail.com>
2025-11-10 06:02:13 +0300

    array intersection challenge

---
+++ level-2-medium/39-array-intersection.js
@@ -0,0 +1,62 @@
+// Write a function to find the intersection of two arrays.
+
+// An intersection is the collection of only common 
+// elements of two arrays.
+
+// For example, consider two arrays [20, 10, 30] and [10, 30, 40, 50]
+// Their intersection is [10, 30]
+
+const arr1 = [10, 20, 30, 40];
+const arr2 = [30, 40, 50, 60];
+console.log(arrayIntersection(arr1, arr2));
+
+function arrayIntersection(arr1, arr2) {
+  const arr = [...arr1, ...arr2];
+  const freq = {};
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const num = arr[i];
+    freq[num] = (freq[num] || 0) + 1;
+  }
+
+  for (key in freq) {
+    if (freq[key] > 1) result.push(+key);
+  }
+
+  return result;
+}
+
+// BEGINNER VERSION
+/*
+function arrayIntersection(arr1, arr2) {
+  const result = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j] && !result.includes(arr1[i])) {
+        result.push(arr1[i]);
+      }
+    }
+  }
+
+  return result;
+}
+
+*/
+
+// ADVANCED: SETS
+/*
+function arrayIntersection(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+
+  const result = [];
+
+  for (let num of set1) {
+    if (set2.has(num)) result.push(num);
+  }
+
+  return result;
+}
+*/
