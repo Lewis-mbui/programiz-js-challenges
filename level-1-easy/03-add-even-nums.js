d5284bf0e9d1d944c73efe6ecdaa66beb6410b2a
Lewis-mbui <lewmugo14479@gmail.com>
2026-01-13 12:48:34 +0300

    changes

--- level-1-easy/03-add-even-nums.js
+++ level-1-easy/03-add-even-nums.js
@@ -6,7 +6,7 @@ console.log(addEvenNumbers(n));
 function addEvenNumbers(n) {
   let sum = 0;
 
-  for (i = 0; i <= n; i++) 
+  for (let i = 0; i <= n; i++) 
     if (i % 2 === 0) sum += i;
 
   return sum;
--- level-2-medium/02-hide-credit-card.js
+++ level-2-medium/02-hide-credit-card.js
@@ -4,17 +4,10 @@ const cardNumber = "1234567890123456"
 
 console.log(hideCreditCard(cardNumber));
 
-/*
-function hideCreditCard(cardNumber) {
-  let newStr = "";
-  for (i = 0; i < cardNumber.length; i++) {
-    if (i < 12) newStr += '*';
-    else newStr += cardNumber[i];
-  }
 
-  return newStr;
+function hideCreditCard(cardNumber) {
+  return "*".repeat(cardNumber.length - 4) + cardNumber.slice(-4);
 }
-*/
 
 /*
 function hideCreditCard(cardNumber) {
@@ -25,7 +18,3 @@ function hideCreditCard(cardNumber) {
   return result;
 }
 */
-
-function hideCreditCard(cardNumber) {
-  return "*".repeat(cardNumber.length - 4) + cardNumber.slice(-4);
-}
